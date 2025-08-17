307d1c80b984888de454909e782572316306f295
Nixxiva <sarahbustinera24@gmail.com>
2025-08-17 21:01:14 +0800

    Add user authentication, devise-jwt integration for signup log in and log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
+
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable,
+        :jwt_authenticatable, jwt_revocation_strategy: self
   
   has_many :dietary_preferences, dependent: :destroy
   has_many :allergies, dependent: :destroy
   has_many :disliked_ingredients, dependent: :destroy
   has_many :health_goals, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, :last_name, :role, presence: true
 end
