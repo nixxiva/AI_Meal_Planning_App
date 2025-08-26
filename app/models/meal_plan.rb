69abe383cec9a8b126e672ab2c0e12a7e983f3ab
Nixxiva <sarahbustinera24@gmail.com>
2025-08-27 01:59:26 +0800

    Added meal plan crud, meal plean recipe, create and destroy

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,3 +1,8 @@
 class MealPlan < ApplicationRecord
   belongs_to :user
+
+  has_many :meal_plan_recipes, dependent: :destroy
+  has_many :recipes, through: :meal_plan_recipes
+  
+  validates :start_date, :end_date, presence: true
 end
