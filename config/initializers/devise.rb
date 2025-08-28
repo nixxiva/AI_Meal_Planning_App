aa0cb77bf4188a59629cb70eb231d59a91316f9a
Nixxiva <sarahbustinera24@gmail.com>
2025-08-28 19:42:58 +0800

    Changes for devise and cors initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -315,10 +315,10 @@ Devise.setup do |config|
   config.jwt do |jwt|
   jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
   jwt.dispatch_requests = [
-    ['POST', %r{^/login$}]
+    ['POST', %r{^/api/v1/login$}]
   ]
   jwt.revocation_requests = [
-    ['DELETE', %r{^/logout$}]
+    ['DELETE', %r{^/api/v1/logout$}]
   ]
   jwt.expiration_time = 1.day.to_i
   end
